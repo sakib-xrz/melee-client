4f2f1f2e3d42e70384a090e6b6eea4c1cd80a12c
sakib-xrz <sakibxrz21@gmail.com>
2024-03-14 20:52:06 +0600

    Refactor checkout page and card components

--- app/(with-sidebar)/(private-routes)/checkout/components/CheckoutCard.jsx
+++ app/(with-sidebar)/(private-routes)/checkout/components/CheckoutCard.jsx
@@ -5,7 +5,7 @@ const CheckoutCard = ({ product }) => {
   const { name, primary_image, selected_stock, unit_price, size } = product;
 
   return (
-    <div className="border rounded-md w-full mb-4 p-3 space-y-4">
+    <div className="border rounded-md w-full mb-4 p-3 space-y-4 bg-background">
       <div className="flex gap-3">
         <Image
           className="w-16 h-16 object-cover rounded-md border border-border aspect-square"
@@ -14,23 +14,23 @@ const CheckoutCard = ({ product }) => {
           width={100}
           height={100}
         />
-        <div className="w-full">
-          <h1 className="line-clamp-1 cursor-pointer hover:underline underline-offset-2">
-            {name}
-          </h1>
-          <p className="text-xs md:text-sm ">Size : {size}</p>
+        <div className="w-full space-y-2">
+          <div className="flex justify-between items-center text-lg">
+            <h1 className="line-clamp-1 cursor-pointer hover:underline underline-offset-2">
+              {name}
+            </h1>
+            <p>
+              ${parseFloat(unit_price).toFixed(2)}
+              <span className="text-gray-500"> x {selected_stock}</span>
+            </p>
+          </div>
+          <div className="flex justify-between items-center text-base">
+            <p>Size : {size}</p>
+            <p className="font-medium">
+              Total ${parseFloat(selected_stock * unit_price).toFixed(2)}
+            </p>
+          </div>
         </div>
-      </div>
-
-      <div className="flex justify-between">
-        <p>
-          ${parseFloat(unit_price).toFixed(2)}
-          <span className="text-gray-500"> x {selected_stock}</span>
-        </p>
-        <p className="font-medium">
-          {" "}
-          ${parseFloat(selected_stock * unit_price).toFixed(2)}
-        </p>
       </div>
     </div>
   );
--- app/(with-sidebar)/(private-routes)/checkout/page.jsx
+++ app/(with-sidebar)/(private-routes)/checkout/page.jsx
@@ -7,7 +7,8 @@ import CheckoutCard from "./components/CheckoutCard";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function CheckOutPage() {
-  const { getCartItems, carts, cartLoading } = useStore();
+  const { getCartItems, carts, cartLoading, user } = useStore();
+  console.log(user);
   const { data: cartData } = GetCart();
 
   useEffect(() => {
@@ -24,18 +25,38 @@ export default function CheckOutPage() {
     <>
       <title>Checkout | MELEE</title>
       <Container>
-        <Card>
-          <CardContent>
-            <CardHeader>
-              <CardTitle className="mb-2">Shopping Cart</CardTitle>
-            </CardHeader>
-            <div className="space-y-4">
-              {checkoutItems.map((product, index) => (
-                <CheckoutCard key={index} product={product} />
-              ))}
+        <h1 className="text-3xl font-bold">Checkout</h1>
+        <p className="mb-6">Please Review All Details before Placing Order</p>
+        <div className="flex gap-6">
+          <Card className="w-full">
+            <CardContent>
+              <CardHeader>
+                <CardTitle>Shopping Cart</CardTitle>
+              </CardHeader>
+              <div className="space-y-4 mt-3">
+                {checkoutItems.map((product, index) => (
+                  <CheckoutCard key={index} product={product} />
+                ))}
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="w-full h-fit">
+            <div>
+              <CardHeader>
+                <CardTitle>Customer Details</CardTitle>
+              </CardHeader>
+              <div className="border rounded-md px-4 py-3 bg-background mt-3">
+                <div className="space-y-2">
+                  <p className="text-base font-bold">
+                    {user?.first_name + " " + user?.last_name}
+                  </p>
+                  <p className="text-sm">{user?.phone}</p>
+                </div>
+              </div>
             </div>
-          </CardContent>
-        </Card>
+            <div></div>
+          </Card>
+        </div>
       </Container>
     </>
   );
