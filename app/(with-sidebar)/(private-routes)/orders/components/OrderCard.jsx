04622a7550cea1c984f68b7fdab4736d4d6dac1b
nusrat-jahan7 <nusrat.jahan03060@gmail.com>
2024-03-15 21:29:59 +0600

    Order page and Order items card created

--- app/(with-sidebar)/(private-routes)/orders/components/OrderCard.jsx
+++ app/(with-sidebar)/(private-routes)/orders/components/OrderCard.jsx
@@ -1,3 +1,30 @@
+import { Card } from "@/components/ui/card";
+import { ClipboardList } from "lucide-react";
+import Link from "next/link";
+
 export default function OrderCard() {
-  return <div>OrderCard</div>;
+  return (
+    <>
+      <Card className="!p-0 !pt-4">
+        <div className="px-4 pb-3">
+          <div className="flex justify-between">
+            <h1 className=" font-semibold">Order #6850031</h1>
+            <small className="bg-green-600 text-white text-xs rounded-full py-0 px-2 flex justify-center items-center">
+              Completed
+            </small>
+          </div>
+          <p className="pt-4 text-gray-400">March 15, 2024, 7:38 PM</p>
+          <p className="text-xl font-semibold mt-1">$ 54.00</p>
+        </div>
+        <hr />
+        <Link
+          href={"/orders/1"}
+          className="flex items-center justify-center pb-3 pt-3 gap-3 px-4 hover:bg-accent rounded-b-md cursor-pointer"
+        >
+          <ClipboardList className="w-5" />
+          <p>View Details</p>
+        </Link>
+      </Card>
+    </>
+  );
 }
--- app/(with-sidebar)/(private-routes)/orders/page.jsx
+++ app/(with-sidebar)/(private-routes)/orders/page.jsx
@@ -1,3 +1,17 @@
+import Container from "@/components/shared/Container";
+import OrderCard from "./components/OrderCard";
+
 export default function OrderPage() {
-  return <div>OrderPage</div>;
+  return (
+    <>
+      <title>Orders | MELEE</title>
+      <Container>
+        <h1 className="text-3xl font-bold">My Orders</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 my-6">
+          <OrderCard />
+          <OrderCard />
+        </div>
+      </Container>
+    </>
+  );
 }
