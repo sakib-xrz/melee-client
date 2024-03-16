3ed3919c1eb1303b078376ea70b009cfb4cae6f5
nusrat-jahan7 <nusrat.jahan03060@gmail.com>
2024-03-16 21:38:40 +0600

    Contact redesign & functionality

--- app/(with-sidebar)/contact/page.jsx
+++ app/(with-sidebar)/contact/page.jsx
@@ -1,6 +1,15 @@
+"use client";
+
 import Container from "@/components/shared/Container";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useStore } from "@/context/StoreProvider";
+import { Mails, Phone, Send } from "lucide-react";
 
 export default function ContactPage() {
+  const { store } = useStore();
+
   return (
     <>
       <title>Contact | MELEE</title>
@@ -14,56 +23,14 @@ export default function ContactPage() {
                 {`We'd love to hear from you! Reach out to us using the
                 information below:`}
               </p>
-              <div className="flex items-center mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mr-2 text-blue-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                  />
-                </svg>
-                <p>123 Main Street, City</p>
+
+              <div className="flex items-center mb-4 gap-2">
+                <Mails />
+                <p>Email: {store?.contact_email}</p>
               </div>
-              <div className="flex items-center mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mr-2 text-blue-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                  />
-                </svg>
-                <p>Email: info@example.com</p>
-              </div>
-              <div className="flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mr-2 text-blue-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                  />
-                </svg>
-                <p>Phone: +1 (555) 123-4567</p>
+              <div className="flex items-center gap-2">
+                <Phone />
+                <p>Phone: {store?.contact_number}</p>
               </div>
             </div>
             {/* Contact Form */}
@@ -74,24 +41,24 @@ export default function ContactPage() {
                   <label htmlFor="name" className="block text-sm font-medium">
                     Name
                   </label>
-                  <input
+                  <Input
                     type="text"
                     id="name"
                     name="name"
-                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                     placeholder="Your name"
+                    className="mt-2"
                   />
                 </div>
                 <div className="mb-4">
                   <label htmlFor="email" className="block text-sm font-medium">
                     Email
                   </label>
-                  <input
+                  <Input
                     type="email"
                     id="email"
                     name="email"
-                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                     placeholder="Your email"
+                    className="mt-2"
                   />
                 </div>
                 <div className="mb-4">
@@ -101,20 +68,18 @@ export default function ContactPage() {
                   >
                     Message
                   </label>
-                  <textarea
+                  <Textarea
                     id="message"
                     name="message"
                     rows="4"
-                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                     placeholder="Your message"
-                  ></textarea>
+                    className="mt-2"
+                  />
                 </div>
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-                >
-                  Send
-                </button>
+                <Button type="submit" className="gap-2">
+                  Send Message
+                  <Send className="w-4 h-4" />
+                </Button>
               </form>
             </div>
           </div>
