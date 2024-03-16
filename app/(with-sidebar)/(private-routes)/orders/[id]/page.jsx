9352d1dca716d2f21c240d0151e5a5f1b9193d6f
sakib-xrz <sakibxrz21@gmail.com>
2024-03-16 11:08:13 +0600

    Refactor OrderStatusTracker component and update OrderDetails page

--- app/(with-sidebar)/(private-routes)/orders/[id]/page.jsx
+++ app/(with-sidebar)/(private-routes)/orders/[id]/page.jsx
@@ -7,7 +7,14 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 import placeholderImage from "@/public/images/placeholder.png";
-import OrderStatusTracker from "./components/OrderStatusTracker";
+
+const statusClasses = {
+  "Order Placed": "bg-gray-600",
+  Processing: "bg-blue-600",
+  "On the Way": "bg-yellow-600",
+  "Out for Delivery": "bg-orange-600",
+  Delivered: "bg-green-600",
+};
 
 export default function OrderDetails() {
   return (
@@ -17,49 +24,45 @@ export default function OrderDetails() {
         <h1 className="text-3xl font-bold">Order Details</h1>
         <p className="mb-6">View Your Order Details Here.</p>
         <Card className="space-y-3">
-          <CardHeader>
-            <CardTitle className="!text-2xl">Order ID: #4458634</CardTitle>
-            <CardDescription>Order date: March 20, 2024</CardDescription>
-          </CardHeader>
+          <div className="space-y-2 xs:flex xs:justify-between xs:items-center">
+            <CardHeader>
+              <CardTitle className="!text-xl xs:!text-2xl">
+                Order ID: #4458634
+              </CardTitle>
+              <CardDescription>Order date: March 20, 2024</CardDescription>
+            </CardHeader>
+            <small
+              className={`${statusClasses["Delivered"]} text-white rounded-md px-4 py-2 xs:text-lg flex justify-center items-center`}
+            >
+              Delivered
+            </small>
+          </div>
 
           <hr />
 
           <div>
-            <OrderStatusTracker status="Delivered" />
-          </div>
-
-          <div>
             <h2 className="text-xl font-bold">Order Items</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-              <div>
+
+            {/* product card start here  */}
+            <div className="grid grid-cols-1 items-end sm:grid-cols-2 xs:gap-4 mt-4">
+              <div className="flex flex-col xs:flex-row gap-2">
                 <Image
                   src={placeholderImage}
                   alt="iPhone 12"
-                  className="w-24 h-24 object-cover rounded-md border"
+                  className="w-20 h-20 object-cover rounded-md border p-2"
                 />
+                <div>
+                  <h3 className="text-lg font-bold">Apple iPhone 12</h3>
+                  <p>Size: M</p>
+                  <p>Price: $799.00</p>
+                </div>
               </div>
               <div>
-                <h3 className="text-lg font-bold">Apple iPhone 12</h3>
-                <p>Price: $799.00</p>
                 <p>Quantity: 1</p>
                 <p>Total: $799.00</p>
               </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-              <div>
-                <Image
-                  src={placeholderImage}
-                  alt="iPhone 12"
-                  className="w-24 h-24 object-cover rounded-md border"
-                />
-              </div>
-              <div>
-                <h3 className="text-lg font-bold">Apple iPhone 12</h3>
-                <p>Price: $799.00</p>
-                <p>Quantity: 1</p>
-                <p>Total: $799.00</p>
-              </div>
-            </div>
+            {/* product card end here  */}
           </div>
 
           <hr />
